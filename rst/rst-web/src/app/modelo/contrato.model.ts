c107e36eb53f0182ab9936969811ee82cd357da2
Jose Roberto Leite da Silva Filho <jose.leite@SLT-002200.solutis.net.br>
2018-11-16 15:12:30 -0300

    Fixes em cadastro de contrato'

--- rst/rst-web/src/app/modelo/contrato.model.ts
+++ rst/rst-web/src/app/modelo/contrato.model.ts
@@ -4,8 +4,8 @@ import {TipoPrograma} from "./tipo-programa.model";
 
 export class  Contrato{
     id: number;
-    dataContratoInicio: Date;
-    dataContratoFim: Date;
+    dataContratoInicio: Date | string;
+    dataContratoFim: Date | string;
     unidadeObra: UnidadeObra;
     anoVigencia: number;
     fl_inativo: string;
