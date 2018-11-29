3800f96b261367930687ab4591bd23be907fb00c
Daiane Silva <daiane.silva@solutis.com.br>
2018-11-29 17:02:55 -0300

    Adição da listagem

--- rst/rst-web/src/app/componente/usuario/manter-usuario/perfis-associados/perfis-associados.component.ts
+++ rst/rst-web/src/app/componente/usuario/manter-usuario/perfis-associados/perfis-associados.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 
 import { Usuario } from 'app/modelo/usuario.model';
+import { UsuarioPerfilSistema } from 'app/modelo/usuario-perfil-sistema.model';
+import { PerfilEnum } from 'app/modelo/enum/enum-perfil';
 
 @Component({
   selector: 'app-perfis-associados',
@@ -10,8 +12,36 @@ export class PerfisAssociadosComponent {
 
   @Input() usuario: Usuario;
 
-  getSistemaPerfil(): any {
-    return [];
-  }
+  public getSistemaPerfil(): any {
+    console.log(this.usuario);
+    const sistemas = {};
+    this.usuario.perfisSistema.forEach((ps) => {
+        const id = ps.sistema.id;
+
+        if (!sistemas[id]) {
+            sistemas[id] = { id, sistema: ps.sistema.nome, perfil: '' };
+        }
+        sistemas[id].perfil = ps.perfil.nome.toString().concat('; ').concat(sistemas[id].perfil);
+    });
+
+    return Object.keys(sistemas).map((key) => {
+        sistemas[key].perfil = sistemas[key].perfil.substr(0, sistemas[key].perfil.length - 2);
+        return sistemas[key];
+    });
+}
+
+excluirAssociacaoPerfil(idSistema: number): void {
+    const sistemaPerfis: UsuarioPerfilSistema[] = this.usuario.perfisSistema.filter((ps) => ps.sistema.id
+        === Number(idSistema));
+    sistemaPerfis.forEach((element) => {
+        this.usuario.perfisSistema;
+        const i = this.usuario.perfisSistema.indexOf(element, 0);
+        if (element.perfil.codigo !== PerfilEnum.TRA) {
+            if (i > -1) {
+                this.usuario.perfisSistema.splice(i, 1);
+            }
+        }
+    });
+}
 
 }
