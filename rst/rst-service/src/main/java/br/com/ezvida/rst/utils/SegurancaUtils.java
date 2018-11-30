3a21ef442db2fbd465cc2b6adfe0854dc080ab74
Daiane Silva <daiane.silva@solutis.com.br>
2018-11-30 09:51:59 -0300

    Deletar

--- rst/rst-service/src/main/java/br/com/ezvida/rst/utils/SegurancaUtils.java
+++ rst/rst-service/src/main/java/br/com/ezvida/rst/utils/SegurancaUtils.java
@@ -29,12 +29,12 @@ public class SegurancaUtils {
             //return "37139762520"; // adm
             // return "09934689421"; // dna
             // return "48552219026"; // gestor dn aplicacoes
-             return "56866951135"; // gestor dr master
-            // return "74055251564"; // gestor dr aplicacoes
+//             return "56866951135"; // gestor dr master
+//             return "74055251564"; // gestor dr aplicacoes
              //return "71141598973"; // gestor empresa master
             // return "92883645361"; // gestor empresa
             // return "46335766744"; // trabalhador
-            // return "37139762520"; // carol adm
+             return "37139762520"; // carol adm
             // return "64432620005"; //profissonal saude
             // return "19743559698"; //recursos humanos
             // return "93693744181"; // MTSDN
