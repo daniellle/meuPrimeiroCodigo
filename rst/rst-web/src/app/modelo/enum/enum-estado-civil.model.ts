f442ff80adc3a90b76e59955df9ffd506462ba90
Tailine Chagas <tailine.jesus@solutis.com.br>
2018-12-26 14:21:29 -0300

    alteracoes no campo de estado civil

--- rst/rst-web/src/app/modelo/enum/enum-estado-civil.model.ts
+++ rst/rst-web/src/app/modelo/enum/enum-estado-civil.model.ts
@@ -1,10 +1,7 @@
 export enum EstadoCivil {
     CA = 'Casado',
-    DQ = 'Desquitado',
     DV = 'Divorciado',
-    OU = 'Outro',
     SO = 'Solteiro',
     SP = 'Separado',
-    UE = 'União Estável',
     VI = 'Viúvo',
 }
