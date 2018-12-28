50471491483f190fedc45def6cc10434149d328b
Daiane Silva <daiane.silva@solutis.com.br>
2018-12-28 09:04:12 -0300

    Criação do component de pesquisa de relatório perfil usuario

--- rst/rst-web/src/app/componente/perfil-usuario/perfil-usuario.module.ts
+++ rst/rst-web/src/app/componente/perfil-usuario/perfil-usuario.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { PerfilUsuarioComponent } from './perfil-usuario.component';
 import { CompartilhadoModule } from 'app/compartilhado/compartilhado.module';
+import { PesquisaPerfilUsuarioComponent } from '../src/app/componente/perfil-usuario/pesquisa-perfil-usuario/pesquisa-perfil-usuario.component';
 
 const routes: Routes = [
   {path: '', component: PerfilUsuarioComponent},
@@ -15,7 +16,7 @@ const routes: Routes = [
       CompartilhadoModule,
       RouterModule,
   ],
-  declarations: [PerfilUsuarioComponent],
+  declarations: [PerfilUsuarioComponent, PesquisaPerfilUsuarioComponent],
   providers: [],
   exports: [PerfilUsuarioComponent]
 })
---
+++ rst/rst-web/src/app/componente/perfil-usuario/pesquisa-perfil-usuario/pesquisa-perfil-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesquisa-perfil-usuario',
+  templateUrl: './pesquisa-perfil-usuario.component.html',
+  styleUrls: ['./pesquisa-perfil-usuario.component.scss']
+})
+export class PesquisaPerfilUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
