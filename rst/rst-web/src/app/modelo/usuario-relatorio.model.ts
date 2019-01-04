8f78291dddb9ccc919def8eb8282b417afaeebbe
Daiane Silva <daiane.silva@solutis.com.br>
2019-01-04 13:48:35 -0300

    Alteração do paginado

--- rst/rst-web/src/app/modelo/usuario-relatorio.model.ts
+++ rst/rst-web/src/app/modelo/usuario-relatorio.model.ts
@@ -1,4 +1,13 @@
+import { UsuarioPerfilSistema } from ".";
+import { DepartamentoRegional } from "./departamento-regional.model";
+import { UnidadeAtendimentoTrabalhador } from "./unid-atend-trabalhador.model";
+import { Empresa } from "./empresa.model";
+
 export class UsuarioRelatorio {
     nome: string;
     login: string;
+    perfisSistema: UsuarioPerfilSistema[];
+    departamentoRegional: DepartamentoRegional;
+    unidadeSesi: UnidadeAtendimentoTrabalhador;
+    empresa: Empresa;
 }
