1e494098a30abd0350c0d783e7f50948fd42e9a9
Matheus Carvalho <matheus.carvalho@solutis.com.br>
2019-02-25 16:37:13 -0300

    Ajuste Entidade Remoção de unique mapeada

--- rst/rst-service/src/main/java/br/com/ezvida/rst/model/UatEquipamentoArea.java
+++ rst/rst-service/src/main/java/br/com/ezvida/rst/model/UatEquipamentoArea.java
@@ -1,5 +1,7 @@
 package br.com.ezvida.rst.model;
 
+import fw.core.model.BaseModel;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,32 +9,46 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "UAT_EQUIPAMENTO_AREA", uniqueConstraints = @UniqueConstraint(name = "PK_UAT_EQUIPAMENTO_AREA", columnNames = {
-"ID_UAT_EQUIPAMENTO_AREA" }))
-public class UatEquipamentoArea extends AbstractData {
-	
-	private static final long serialVersionUID = 1L;
+@Table(name = "uat_equipamento_area")
+public class UatEquipamentoArea implements BaseModel<Long> {
 
-	@Id
-	@Column(name = "ID_UAT_EQUIPAMENTO_AREA")
-    private Long id; 
-	
-	private String descricao;
+    private static final long serialVersionUID = 7756440197166224569L;
 
-	public Long getId() {
-		return id;
-	}
+    @Id
+    @Column(name = "id_uat_equipamento_area")
+    private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @Column(name = "descricao")
+    private String descricao;
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UatEquipamentoArea that = (UatEquipamentoArea) o;
+
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
