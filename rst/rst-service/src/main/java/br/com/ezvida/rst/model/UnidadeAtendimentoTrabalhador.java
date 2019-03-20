36d2faa9e08c89f2cd98d520da88469f8aa591d4
Antonio Carlos dos Santos Costa <antonio.costa@SLT-002197.solutis.net.br>
2019-03-20 14:48:03 -0300

    Retornando propriedade estado em empresa->uat->departamentoRegional

--- rst/rst-service/src/main/java/br/com/ezvida/rst/model/UnidadeAtendimentoTrabalhador.java
+++ rst/rst-service/src/main/java/br/com/ezvida/rst/model/UnidadeAtendimentoTrabalhador.java
@@ -93,11 +93,10 @@ public class UnidadeAtendimentoTrabalhador extends AbstractData {
 		this.id = idUat;
 	}
 
-    public UnidadeAtendimentoTrabalhador(String cnpj, String razaoSocial, String nomeFantasia, DepartamentoRegional dr) {
+    public UnidadeAtendimentoTrabalhador(String cnpj, String razaoSocial, DepartamentoRegional dr) {
         this.cnpj = cnpj;
         this.razaoSocial = razaoSocial;
-        this.nomeFantasia = nomeFantasia;
-        this.departamentoRegional = new DepartamentoRegional(dr.getCnpj(), dr.getRazaoSocial(), dr.getSiglaDR());
+        this.departamentoRegional = new DepartamentoRegional(dr.getCnpj(), dr.getRazaoSocial(), dr.getSiglaDR(), dr.getSiglaDR());
     }
 
 
