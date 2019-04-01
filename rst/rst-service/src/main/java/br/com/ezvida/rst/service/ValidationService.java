d701a8bb066d1f172b7dbc1a207dbf1e38f94f6c
Matheus Carvalho <matheus.carvalho@solutis.com.br>
2019-04-01 09:54:32 -0300

    Testes de validação filtro de dados

--- rst/rst-service/src/main/java/br/com/ezvida/rst/service/ValidationService.java
+++ rst/rst-service/src/main/java/br/com/ezvida/rst/service/ValidationService.java
@@ -17,40 +17,30 @@ public class ValidationService {
     private UnidadeObraContratoUatService unidadeObraContratoUatService;
 
     public boolean validarFiltroDadosGestaoUnidadeSesi(DadosFilter dados, Long idUnidade) {
-        boolean next = false;
         if (dados.isAdministrador() || dados.isGestorDn()) {
-            next = true;
+            return true;
         } else if (dados.isGestorDr()) {
             Set<Long> listIdDepartamentosRegionais = dados.getIdsDepartamentoRegional();
-            if (listIdDepartamentosRegionais != null && !listIdDepartamentosRegionais.isEmpty() && unidadeAtendimentoTrabalhadorService.existsByListDRIdAndIdUnidade(new ArrayList<>(listIdDepartamentosRegionais), idUnidade)) {
-                next = true;
-            }
+            return listIdDepartamentosRegionais != null && !listIdDepartamentosRegionais.isEmpty() && unidadeAtendimentoTrabalhadorService.existsByListDRIdAndIdUnidade(new ArrayList<>(listIdDepartamentosRegionais), idUnidade);
         } else if (dados.isGetorUnidadeSESI()) {
             Set<Long> listIdUnidades = dados.getIdsUnidadeSESI();
-            if (listIdUnidades != null && !listIdUnidades.isEmpty() && listIdUnidades.contains(idUnidade)) {
-                next = true;
-            }
+            return listIdUnidades != null && !listIdUnidades.isEmpty() && listIdUnidades.contains(idUnidade);
         }
 
-        return next;
+        return false;
     }
 
     public boolean validarFiltroDadosContrato(DadosFilter dados, Long idContrato) {
-        boolean next = false;
         if (dados.isSuperUsuario()) {
-            next = true;
+            return true;
         } else if (dados.isGestorDr()) {
             Set<Long> listIdDepartamentosRegionais = dados.getIdsDepartamentoRegional();
-            if (listIdDepartamentosRegionais != null && !listIdDepartamentosRegionais.isEmpty() && unidadeObraContratoUatService.existisByDrs(new ArrayList<>(listIdDepartamentosRegionais), idContrato)) {
-                next = true;
-            }
+            return listIdDepartamentosRegionais != null && !listIdDepartamentosRegionais.isEmpty() && unidadeObraContratoUatService.existisByDrs(new ArrayList<>(listIdDepartamentosRegionais), idContrato);
         } else if (dados.isGetorUnidadeSESI()) {
             Set<Long> listIdUnidades = dados.getIdsUnidadeSESI();
-            if (listIdUnidades != null && !listIdUnidades.isEmpty() && unidadeObraContratoUatService.existisByUnidades(new ArrayList<>(listIdUnidades), idContrato)) {
-                next = true;
-            }
+            return listIdUnidades != null && !listIdUnidades.isEmpty() && unidadeObraContratoUatService.existisByUnidades(new ArrayList<>(listIdUnidades), idContrato);
         }
 
-        return next;
+        return false;
     }
 }
